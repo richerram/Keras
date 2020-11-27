89fe6fdea8eaa043b871a3218975495316f069ad
Ricardo Hernandez Ramon <richerram@gmail.com>
2020-11-27 01:36:05 -0600

    Week 2 Convolutional and Pooling Layers

--- Keras.py
+++ Keras.py
@@ -1,9 +1,11 @@
 import tensorflow
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten, Softmax
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
 
-model = Sequential(
-    [Flatten (input_shape=(28,28)),
-     Dense(16, activation = "relu"),
-     Dense(16, activation = "relu"),
-     Dense(10, activation ="softmax")])
+model = Sequential ([
+    Conv2D (16, (3,3), padding = "SAME", strides = 2, activation = "relu", input_shape= (32, 32, 3)),
+    MaxPooling2D ((3,3)),
+    Flatten (),
+    Dense (16, activation = "relu"),
+    Dense (10, activation = "softmax")
+])
