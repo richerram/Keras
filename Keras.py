80b3a80d78240a932857c41bd2ac7e04ec6f6043
Ricardo Hernandez Ramon <richerram@gmail.com>
2020-11-28 20:51:22 -0600

    Validation split (sktlearn) and Overfitting

--- Keras.py
+++ Keras.py
@@ -1,42 +1,47 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, MaxPooling2D, Conv2D, Flatten
-from tensorflow.keras.preprocessing import image
+from tensorflow.keras.layers import Dense
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
 
+diabetes = load_diabetes()
 
-model = Sequential ([Conv2D(16,(3,3), activation="relu", input_shape=(28,28,1)),
-                    MaxPooling2D((3,3)),
-                    Flatten(),
-                    Dense(10,activation="softmax")])
+#print (diabetes["DESCR"])
+#print (diabetes.keys())
 
-opt = tf.keras.optimizers.Adam(learning_rate=0.005)
-acc = tf.keras.metrics.SparseCategoricalAccuracy()
-mae = tf.keras.metrics.MeanAbsoluteError()
+data = diabetes["data"]
+target = diabetes["target"]
 
-model.compile(optimizer=opt, loss="sparse_categorical_crossentropy", metrics=[acc, mae])
+target = (target - target.mean(axis=0)) / target.std()
+train_x, test_x, train_y, test_y = train_test_split(data, target, test_size=0.1)
 
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
+print ("train_x: {}\ntest_x: {}\ntrain_y: {}\ntest_y: {}".format(train_x.shape, test_x.shape, train_y.shape, test_y.shape))
 
-labels = ["t-shirts", "trouser", "pullover", "dress", "coat", "sandal", "shirt", "sneaker", "bag", "boot"]
+def get_model():
+    model = Sequential([
+        Dense(128, activation="relu", input_shape=(train_x.shape[1],)),
+        Dense(128, activation="relu"),
+        Dense(128, activation="relu"),
+        Dense(128, activation="relu"),
+        Dense(128, activation="relu"),
+        Dense(128, activation="relu"),
+        Dense(1)
+    ])
+    return model
 
-x_train = x_train / 255
-x_train = x_train[...,np.newaxis]
-x_test = x_test / 255
-x_test = x_test[...,np.newaxis]
+model=get_model()
+model.compile(optimizer="adam", loss="mse", metrics=["mae"])
+hist = model.fit(train_x, train_y, epochs=100, validation_split=0.15, batch_size=64, verbose=False)
+model.evaluate(test_x, test_y, verbose=2)
 
-# i = 43
-# img = x_train[i, :, :]
-# plt.imshow(img)
-# plt.show()
-# print (f"label: {labels[y_train[i]]}")
+#PLOTTING
+plt.plot(hist.history["loss"])
+plt.plot(hist.history["val_loss"])
+plt.title("Loss vs Validation Loss")
+plt.ylabel("Loss")
+plt.xlabel("Epochs")
+plt.legend(["Training", "Validation"], loc="upper right")
+plt.show()
 
-#history = model.fit(x_train, y_train, epochs=10, verbose=2)
-df = pd.DataFrame(history.history)
-
-loss_plot = df.plot(y="loss")
-
-model.evaluate(x_test, y_test)
-model.predict(x_test[49])
+#THIS IS OVERFITTING
