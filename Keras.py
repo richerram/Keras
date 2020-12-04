de8e73245df0406d82f6c72d0baba0985153cf44
Ricardo Hernandez Ramon <richerram@gmail.com>
2020-12-03 23:02:22 -0600

    Importing ResNet50 example

--- Keras.py
+++ Keras.py
@@ -1,66 +1,15 @@
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
-from tensorflow.keras.callbacks import ModelCheckpoint
-from tensorflow.keras.losses import BinaryCrossentropy
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+import numpy as np
 
-(train_x, train_y), (test_x, test_y) = tf.keras.datasets.cifar10.load_data()
-train_x = train_x/ 255.0
-test_x = test_x / 255.0
+model = ResNet50()
+print (model.summary())
 
-train_x = train_x [:10000]
-train_y = train_y [:10000]
-test_x = test_x [:1000]
-test_y = test_y [:1000]
+img_input = image.load_img ('me.jpg', target_size=(224,224))
+img_input = image.img_to_array (img_input)
+img_input = preprocess_input(img_input[np.newaxis,...])
 
-import matplotlib.pyplot as plt
-
-# fig, ax = plt.subplots(1, 10, figsize=(10,1))
-# for i in range(10):
-#     ax[i].set_axis_off()
-#     ax[i].imshow(train_x[i])
-
-def get_test_accuracy (model, x, y):
-    test_loss, test_accuracy = model.evaluate (x, y, verbose=0)
-    print (f'accuracy {test_accuracy}')
-
-def get_model():
-    model = Sequential([
-        Conv2D(filters=16, input_shape=(32,32,3), kernel_size=(3,3), activation='relu', name='conv_1'),
-        Conv2D(filters=8, activation='relu', kernel_size=(3,3), name='conv_2'),
-        MaxPooling2D(pool_size=(4,4), name='pool_1'),
-        Flatten(name='flatten_1'),
-        Dense(32, activation='relu', name='dense_1'),
-        Dense(units=10, activation='softmax', name='dense_2' )
-    ])
-
-    model.compile(optimizer="adam", loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
-    return model
-
-model = get_model()
-print (get_test_accuracy(model, test_x, test_y))
-
-############## CHECKPOINT #######################
-#################################################
-checkpoint_path = 'model_checkpoints/checkpoint'
-checkpoint = ModelCheckpoint(filepath=checkpoint_path, frequency='epoch', save_weights_only=True, verbose=False)
-
-model.fit (train_x, train_y, epochs=100, callbacks=[checkpoint], verbose=False)
-
-model.load_weights(checkpoint_path)
-print(get_test_accuracy(model, test_x, test_y))
-
-######################More Examples########################
-'''
-checkpoint5000 = ModelCheckpoint(filepath='checkpoint_path_5000',
-                                 save_weights_only=True,
-                                 save_freq=5000,
-                                 verbose=1)
-checkpointBEST = ModelCheckpoint(filepath='checkpoint_path_BEST',
-                                 save_weights_only=True,
-                                 save_best_only=True,
-                                 save_freq='epoch',
-                                 monitor='val_accuracy',
-                                 verbose=1)
-'''
+preds = model.predict(img_input)
+decoded_pred = decode_predictions(preds, top=3)[0]
+print (decoded_pred)
