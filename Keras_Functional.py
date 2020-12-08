e92f79e43999461be5a9dddb1411903b5282e981
Ricardo Hernandez Ramon <richerram@gmail.com>
2020-12-08 01:11:05 -0600

    Example with many inputs and 2 outpus

--- Keras_Functional.py
+++ Keras_Functional.py
@@ -1,35 +1,87 @@
-from tensorflow.keras.layers import Input, Dense, Flatten, Conv1D, AveragePooling1D
-from tensorflow.keras.models import Model
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from tensorflow.keras import Input, layers
 
-# Example using LISTS as parameters, all parameters are assigned in the same order as they were created in the MODEL.
+# Loading data
+pd_dat = pd.read_csv('diagnosis.csv')
+dataset = pd_dat.values
 
-inputs = Input(shape=(32,1))
-h = Conv1D(16,5, activation='relu')(inputs)
-h = AveragePooling1D(3)(h)
-h = FLatten()(h)
-aux_inputs = Input(shape=(12,))
-h = Concatenate()([h, aux_inputs])
-outputs =  Dense(20, activation='sigmoid')(h)
-aux_outputs = Dense(1, activation='linear')(h)
+#Splitting data
+train_x, test_x, train_y, test_y = train_test_split(dataset[:,:6], dataset[:,6:], test_size=0.33)
 
-model = Model(inputs=[inputs, aux_inputs], outputs=[outputs, aux_outputs])
-mode.compile (loss=['binary_crossentropy', 'mse'], loss_weights=[1,0.4], metrics=['accuracy'])
-history = model.fit([X_train, X_aux], [y_train, y_aux], validation_split=0.2, epochs=20)
+# Creating individual input and objective matrices
+temp_train, nocc_train, lumbp_train, up_train, mict_train, bis_train = np.transpose(train_x)
+X_train = [temp_train, nocc_train, lumbp_train, up_train, mict_train, bis_train]
+temp_test, nocc_test, lumbp_test, up_test, mict_test, bis_test = np.transpose(test_x)
+X_test = [temp_test, nocc_test, lumbp_test, up_test, mict_test, bis_test]
 
-##########################################################################################
-# Example using DICTIONARIES
+inflam_train, nephr_train = train_y[:,0], train_y[:,1]
+y_train = [inflam_train, nephr_train]
+inflam_test, nephr_test = test_y[:,0], test_y[:,1]
+y_test = [inflam_test, nephr_test]
 
-inputs = Input(shape=(32,1), name='inputs')
-h = Conv1D(16,5, activation='relu')(inputs)
-h = AveragePooling1D(3)(h)
-h = FLatten()(h)
-aux_inputs = Input(shape=(12,), name='aux_inputs')
-h = Concatenate()([h, aux_inputs])
-outputs =  Dense(20, activation='sigmoid', name='outputs')(h)
-aux_outputs = Dense(1, activation='linear', name='aux_outputs')(h)
+############## Creating model #################
+# First we create INPUT layers to then concatenate to 'x' so they can be fed into the 2nd layer (DENSE) as only 1 input (x).
+shape_inputs = (1,)
+temperature = Input(shape=shape_inputs, name='temp')
+nausea_occurence = Input(shape=shape_inputs, name='nocc')
+lumbar_pain = Input(shape=shape_inputs, name='lumbp')
+urine_pushing = Input(shape=shape_inputs, name='up')
+micturition_pain = Input(shape=shape_inputs, name='mict')
+bis = Input(shape=shape_inputs, name='bis')
 
-model = Model(inputs=[inputs, aux_inputs], outputs=[outputs, aux_outputs])
-model.compile(loss={'outputs':'binary_crossentropy', 'aux_outputs':'mse'}, loss_weights={'outputs':1, 'aux_outputs':0.4}, metrics=['accuracy'])
-history = model.fit({'inputs':X_train, 'aux_inputs':X_aux},
-                    {'outputs':y_train, 'aux_outputs':y_aux},
-                    epochs=20, validation_split=20)
+# Concatenation and then split and merge again.
+list_inputs = [temperature, nausea_occurence, lumbar_pain, urine_pushing, micturition_pain, bis]
+x = layers.concatenate(list_inputs)
+xa = layers.Dense(1, activation='relu')(x)
+xb = layers.Dense(1, activation='sigmoid')(x)
+xc = layers.concatenate([xa,xb])
+
+# Creation of the 2 ""model classifiers/outputs"", one for each disease
+inflamation_prediction = layers.Dense(1, activation='sigmoid', name='inflam')(xc)
+nephritis_prediction = layers.Dense(1, activation='sigmoid', name='nephr')(xc)
+list_outputs = [inflamation_prediction, nephritis_prediction]
+
+# Creating MODEL object
+model = tf.keras.Model(inputs=list_inputs, outputs=list_outputs)
+
+# Plotting the model
+tf.keras.utils.plot_model(model, 'multi_input_output_model.png', show_shapes=True)
+
+# Compiling the model.
+model.compile(optimizer=tf.keras.optimizers.RMSprop(1e-3),
+              loss={'inflam':'binary_crossentropy', 'nephr':'binary_crossentropy'},
+              metrics={'inflam':['acc'], 'nephr':['acc']},
+              loss_weights=[1,0.2])
+#Fit test
+history = model.fit(X_train, y_train, epochs=1000, batch_size=128, verbose=False)
+
+#PLOT
+acc_keys = [k for k in history.history.keys() if k in ('inflam_acc', 'nephr_acc')]
+loss_keys = [k for k in history.history.keys() if k not in acc_keys]
+
+for k, v in history.history.items():
+    if k in acc_keys:
+        plt.figure(1)
+        plt.plot(v)
+    else:
+        plt.figure(2)
+        plt.plot(v)
+
+plt.figure(1)
+plt.title('Accuracy vs Epoch')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend(acc_keys)
+
+plt.figure(2)
+plt.title('Loss vs Epochs')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend(loss_keys)
+
+#Evaluate model with the Test Set
+model.evaluate(X_test, y_test, verbose=2)
