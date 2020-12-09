872308e8a4ee17fbd7a3f6da3b705b4869bce5b2
Ricardo Hernandez Ramon <richerram@gmail.com>
2020-12-08 22:47:14 -0600

    Tensor Variables and Operations

--- Keras_Functional.py
+++ Keras_Functional.py
@@ -1,87 +1,120 @@
 import numpy as np
-import pandas as pd
 import tensorflow as tf
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from tensorflow.keras import Input, layers
 
-# Loading data
-pd_dat = pd.read_csv('diagnosis.csv')
-dataset = pd_dat.values
+strings = tf.Variable (["Hello World!"], tf.string)
+floats = tf.Variable ([3.14159, 2.71828], tf.float64)
+ints = tf.Variable ([767,], tf.int32)
+complexs = tf.Variable ([25.9 - 12.4j], tf.complex128)
 
-#Splitting data
-train_x, test_x, train_y, test_y = train_test_split(dataset[:,:6], dataset[:,6:], test_size=0.33)
+tf.Variable(tf.constant(4.2, shape=(3,3)))
 
-# Creating individual input and objective matrices
-temp_train, nocc_train, lumbp_train, up_train, mict_train, bis_train = np.transpose(train_x)
-X_train = [temp_train, nocc_train, lumbp_train, up_train, mict_train, bis_train]
-temp_test, nocc_test, lumbp_test, up_test, mict_test, bis_test = np.transpose(test_x)
-X_test = [temp_test, nocc_test, lumbp_test, up_test, mict_test, bis_test]
+v = tf.Variable(0.0)
+w = v+1
+print (type(w))
 
-inflam_train, nephr_train = train_y[:,0], train_y[:,1]
-y_train = [inflam_train, nephr_train]
-inflam_test, nephr_test = test_y[:,0], test_y[:,1]
-y_test = [inflam_test, nephr_test]
+v.assign_add(1)
+print(v)
 
-############## Creating model #################
-# First we create INPUT layers to then concatenate to 'x' so they can be fed into the 2nd layer (DENSE) as only 1 input (x).
-shape_inputs = (1,)
-temperature = Input(shape=shape_inputs, name='temp')
-nausea_occurence = Input(shape=shape_inputs, name='nocc')
-lumbar_pain = Input(shape=shape_inputs, name='lumbp')
-urine_pushing = Input(shape=shape_inputs, name='up')
-micturition_pain = Input(shape=shape_inputs, name='mict')
-bis = Input(shape=shape_inputs, name='bis')
+v.assign_sub(1)
+print(v)
 
-# Concatenation and then split and merge again.
-list_inputs = [temperature, nausea_occurence, lumbar_pain, urine_pushing, micturition_pain, bis]
-x = layers.concatenate(list_inputs)
-xa = layers.Dense(1, activation='relu')(x)
-xb = layers.Dense(1, activation='sigmoid')(x)
-xc = layers.concatenate([xa,xb])
+x = tf.constant([[3,6,5], [9,3,7], [4,9,2]])
+print(x)
+print ('dtype', x.dtype)
+print ('shape', x.shape)
 
-# Creation of the 2 ""model classifiers/outputs"", one for each disease
-inflamation_prediction = layers.Dense(1, activation='sigmoid', name='inflam')(xc)
-nephritis_prediction = layers.Dense(1, activation='sigmoid', name='nephr')(xc)
-list_outputs = [inflamation_prediction, nephritis_prediction]
+x.numpy()
+print (x)
 
-# Creating MODEL object
-model = tf.keras.Model(inputs=list_inputs, outputs=list_outputs)
+x = tf.constant([[3,6,5], [9,3,7], [4,9,2]], dtype=tf.float32)
+print (x.dtype)
 
-# Plotting the model
-tf.keras.utils.plot_model(model, 'multi_input_output_model.png', show_shapes=True)
+coeffs = np.arange(16)
+print (coeffs)
+shape1 = [4,4]
+shape2 = [8,2]
+shape3 = [2,2,2,2]
+a = tf.constant(coeffs, shape=shape1)
+print ('\n a: \n',a)
+b = tf.constant(coeffs, shape=shape2)
+print ('\n a: \n',b)
+c = tf.constant(coeffs, shape=shape3)
+print ('\n a: \n',c)
 
-# Compiling the model.
-model.compile(optimizer=tf.keras.optimizers.RMSprop(1e-3),
-              loss={'inflam':'binary_crossentropy', 'nephr':'binary_crossentropy'},
-              metrics={'inflam':['acc'], 'nephr':['acc']},
-              loss_weights=[1,0.2])
-#Fit test
-history = model.fit(X_train, y_train, epochs=1000, batch_size=128, verbose=False)
+t = tf.constant(np.arange(80), shape=[5,2,8])
+rank = tf.rank(t)
+print ('rank: ',rank)
+t2 = tf.reshape(t, [8,10])
+print ('t2.shape: ', t2.shape)
 
-#PLOT
-acc_keys = [k for k in history.history.keys() if k in ('inflam_acc', 'nephr_acc')]
-loss_keys = [k for k in history.history.keys() if k not in acc_keys]
+ones = tf.ones(shape=(2,3))
+print ('ones: ',ones)
+zeros = tf.zeros(shape=(3,2))
+print ('zeros: ',zeros)
+eye = tf.eye(3)
+print ('eye: ', eye)
+tensor7 = tf.constant(7.0, shape=[2,3])
+print ('tensor7: ', tensor7)
 
-for k, v in history.history.items():
-    if k in acc_keys:
-        plt.figure(1)
-        plt.plot(v)
-    else:
-        plt.figure(2)
-        plt.plot(v)
+t1 = tf.ones(shape=(2,2))
+t2 = tf.zeros(shape=(2,2))
+concat0 = tf.concat([t1,t2], 0)
+print ('concat0: ', concat0)
+concat1 = tf.concat([t1,t2], 1)
+print('concat1: ', concat1)
 
-plt.figure(1)
-plt.title('Accuracy vs Epoch')
-plt.xlabel('Epochs')
-plt.ylabel('Accuracy')
-plt.legend(acc_keys)
+#Expanding a tensor (like adding an axis on NumPy...np.newaxis...)
+t = tf.constant(np.arange(24), shape=(3,2,4))
+t1 = tf.expand_dims(t,0)
+print(f't1.shape: {t1.shape}\n t1: {t1}')
+t2 = tf.expand_dims(t,1)
+print(f't2.shape: {t2.shape}\n t1: {t2}')
+t3 = tf.expand_dims(t,3)
+print(f't3.shape: {t3.shape}\n t1: {t3}')
 
-plt.figure(2)
-plt.title('Loss vs Epochs')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.legend(loss_keys)
+#Sqeezing a tensor
+t1 = tf.squeeze(t1,0)
+print (f't1.shape back to normal: {t1.shape}')
+t2 = tf.squeeze(t2,1)
+print (f't2.shape back to normal: {t2.shape}')
+t3 = tf.squeeze(t3,3)
+print (f't3.shape back to normal: {t3.shape}')
 
-#Evaluate model with the Test Set
-model.evaluate(X_test, y_test, verbose=2)
+#Slicing a tensor
+x = tf.constant([1,2,3,4,5,6,7])
+print(x[1:4])
+
+#Doing MATH
+c = tf.constant([[1.0,2.0],[3.0,4.0]])
+d = tf.constant([[1.0,1.0], [0.0,1.0]])
+
+matmul_cd = tf.matmul(c,d)
+print(f'matmul: {matmul_cd}')
+
+c_times_d = c*d
+c_plus_d = c+d
+c_minus_d = c-d
+c_div_d = c/d
+print(f'c*d: {c_times_d}\nc+d: {c_plus_d}\nc-d: {c_minus_d}\nc/d: {c_div_d}')
+
+a = tf.constant([[2,3], [3,5]])
+b = tf.constant([[2,3], [4,3]])
+x = tf.constant([[-6.89 + 1.78j], [-2.54 + 2.15j]])
+
+absx = tf.abs(x)
+print (f'absx: {absx}')
+
+powab = tf.pow(a,b)
+print(f'powab: {powab}')
+
+#Normal Distribution tensor
+tn = tf.random.normal(shape=(2,2), mean=0, stddev=1.)
+print (f'Normal Distribution: {tn}')
+
+#Uiform Distribution tensor
+tu = tf.random.uniform(shape=(2,2), minval=0, maxval=10, dtype='int32')
+print(f'Uniform Distribution: {tu}')
+
+#Poisson Distribution tensor
+tp = tf.random.poisson((2,2), 5)
+print(f'Poisson Distribution: {tp}')
