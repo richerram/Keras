ef394c572173d68cd36098e50c07f86a615aff13
Ricardo Hernandez Ramon <richerram@gmail.com>
2020-12-24 10:56:07 -0600

    Padding and Masking IMDB Dataset

--- TF_pipeline.py
+++ TF_pipeline.py
@@ -1,58 +1,56 @@
-##### Processing sequence data (Padding and Masking)#####
+##### Padding and Masking the IMDB dataset #####
 
 import tensorflow as tf
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.layers import Masking
+from tensorflow.keras.datasets import imdb
 import numpy as np
 
-test_input = [[4, 12, 33, 18],
-            [63, 23, 54, 30, 19, 3],
-            [43, 37, 11, 33, 15]]
+(x_train, y_train), (x_test, y_test) = imdb.load_data()
+print(type(x_train))
+# <class 'numpy.ndarray'>
+print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
+# (25000,) (25000,) (25000,) (25000,)
 
-preprocessed_data = pad_sequences(test_input, padding='pre')
-''' [[ 0  0  4 12 33 18]
-    [63 23 54 30 19  3]
-    [ 0 43 37 11 33 15]]'''
-preprocessed_data = pad_sequences(test_input, padding='post')
-''' [[ 4 12 33 18  0  0]
-    [63 23 54 30 19  3]
-    [43 37 11 33 15  0]]'''
-preprocessed_data = pad_sequences(test_input, padding='post', maxlen=5)
-''' [[ 4 12 33 18  0]
-    [23 54 30 19  3]
-    [43 37 11 33 15]]'''
-preprocessed_data = pad_sequences(test_input, padding='post', maxlen=5, truncating='post', value=-1)
-''' [[ 4 12 33 18 -1]
-    [63 23 54 30 19]
-    [43 37 11 33 15]]'''
+# Let's display the first element of the dataset.
+print(x_train[0])
+''' [1, 14, 22, 16, 43, 530, 973, 1622, 1385, 65, 458, 
+    4468, 66, 3941, 4, 173, 36, 256, 5, 25, 100, 43, 
+    838, 112, 50, 670, 22665, 9, 35, 480, 284, 5, 150,...'''
+print(y_train[0])
+# 1 ---> this means it is a positive review
 
-# Now we are going to use the Masking Layer if we want to ignore the sections that are padded (the "0's" added.
-preprocessed_data = pad_sequences(test_input, padding='post')
+# Import the actual words represented by those indexes.
+jsonwords = imdb.get_word_index()
+# If we want to map the words to the indices we need to consider that the "index_from" starts from 3 in the dataset,
+# why this is not mentioned anywhere in the documentation? I don't know!!!
+index_from = 3
+jsonwords = {k:v + index_from for k,v in jsonwords.items()}
 
-masking_layer = Masking(mask_value=0)
-preprocessed_data = preprocessed_data[..., np.newaxis] # the Layer is expecting a 3-dimensional input so... (batch_size, seq_length, features).
-''' [[[ 4] [12] [33] [18] [ 0] [ 0]]
-    [[63] [23] [54] [30] [19] [ 3]]
-    [[43] [37] [11] [33] [15] [ 0]]]'''
+# Let's print the sentence.
+print(*[k for i in x_train[0] for k,v in jsonwords.items() if v==i])
+'''
+this film was just brilliant casting location scenery story direction everyone's really suited the part they played and 
+you could just imagine being there robert redford's is an amazing actor and now the same being director norman's father
+came from the same scottish island as myself so i loved the fact there was a real connection with this film the witty 
+remarks throughout the film were great it was just brilliant so much that i bought the film as soon as it was released 
+for retail and would recommend it to everyone to watch and the fly fishing was amazing really cried at the end it was 
+so sad and you know what they say if you cry at a film it must have been good and this definitely was also congratulations 
+to the two little boy's that played the part's of norman and paul they were just brilliant children are often left out 
+of the praising list i think because the stars that play them all grown up are such a big profile for the whole film but 
+these children are amazing and should be praised for what they have done don't you think the whole story was so lovely 
+because it was true and was someone's life after all that was shared with us all
+'''
 
-masked_input = masking_layer(preprocessed_data)
-''' tf.Tensor(  [[[ 4] [12] [33] [18] [ 0] [ 0]]
-                [[63] [23] [54] [30] [19] [ 3]]
-                [[43] [37] [11] [33] [15] [ 0]]], shape=(3, 6, 1), dtype=int32)'''
+print('GPU name: {}', format(tf.test.gpu_device_name()))
 
-masked_input._keras_mask
-''' <tf.Tensor: shape=(3, 6), dtype=bool, numpy= array( 
-[[ True,  True,  True,  True, False, False],
-[ True,  True,  True,  True,  True,  True],
-[ True,  True,  True,  True,  True, False]])>'''
+# Not all sequences are the same length so we are going to PAD them.
+pad_x_train =  tf.keras.preprocessing.sequence.pad_sequences(x_train, maxlen=300, padding='post', truncating='pre')
+print(pad_x_train.shape)
 
-
-# All this easily works on lists of lists
-test_input = [[[2,1], [3,3]],
-              [[4,3], [2,4], [1,1]]]
-
-preprocessed_data = pad_sequences(test_input, padding='post')
-''' [[[2 1] [3 3] [0 0]]
-    [[4 3] [2 4] [1 1]]]'''
-
+# We will MASK these, fot that we will create (batch, sequence and features... so we need to add an extra array layer.
+# We also need to convert it to TENSOR before masking it.
+pad_x_train = np.expand_dims(pad_x_train, -1)
+tf_x_train = tf.convert_to_tensor(pad_x_train, dtype='float32')
+masking_layer = tf.keras.layers.Masking(mask_value=0.0)
+mask_x_train = masking_layer(tf_x_train)
+print(mask_x_train._keras_mask)
 
