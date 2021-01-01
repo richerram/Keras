f3eaf7ed525ad8bb0c0bcdbf54f2899ad42ce7d6
Ricardo Hernandez Ramon <richerram@gmail.com>
2021-01-01 17:51:14 -0600

    Flexible Input Shapes

--- TF_pipeline.py
+++ TF_pipeline.py
@@ -1,94 +1,74 @@
-##### Custom Layers #####
+##### Flexible Input Shapes #####
 import tensorflow as tf
 from tensorflow.keras.layers import Layer, Softmax
 from tensorflow.keras.models import Model
 
 class MyLayer(Layer):
-    def __init__(self, units, input_dim):
-        super(MyLayer, self).__init__()
+    def __init__(self, units, input_dim, **kwargs):
+        super(MyLayer, self).__init__(**kwargs)
         self.w = self.add_weight(shape=(input_dim, units), initializer='random_normal')
-        self.b = self.add_weight(shape=(units,), initializer='zeros')
+        self.b = self.add_weight(shape=(units,), initializer='random_normal')
 
     def call(self, inputs):
-        return tf.matmul(inputs, self.w)+self.b
+        return tf.matmul(inputs, self.w) + self.b
 
+# Notice defining the function this way we need to specifiy this values when we instantiete the layer.
 dense_layer = MyLayer(3,5)
 x = tf.ones((1,5))
 print(dense_layer(x))
-print(dense_layer.weights)
+# tf.Tensor([[0.1397101  0.13544825 0.01942875]], shape=(1, 3), dtype=float32)
 
 
-# We can also specify if we want to make the Weights and Bias trainable or not.
+### Allowing Flexible Inputs using the "build" method. This is called "lazy" weight creation. The "build" method
+# is executed automatically after the "call" method is called.
 class MyLayer(Layer):
-    def __init__(self, units, input_dim):
-        super(MyLayer, self).__init__()
-        self.w = self.add_weight(shape=(input_dim, units), initializer='random_normal', trainable=False)
-        self.b = self.add_weight(shape=(units,), initializer='zeros', trainable=False)
+    def __init__(self, units, **kwargs):
+        super(MyLayer, self).__init__(**kwargs)
+        self.units = units
+
+    def build(self, input_shape):
+        self.w = self.add_weight(shape=(input_shape[-1], self.units), initializer='random_normal')
+        self.b = self.add_weight(shape=(self.units,), initializer='zeros')
 
     def call(self, inputs):
-        return tf.matmul(inputs, self.w)+self.b
+        return tf.matmul(inputs, self.w) + self.b
 
-dense_layer = MyLayer(3,5)
-print('Trainable Weights: ', len(dense_layer.trainable_weights))
-print('Non-Trainable Weights: ', len(dense_layer.non_trainable_weights))
+dense_layer = MyLayer(3)
+x = tf.ones((1,5))
+print(dense_layer(x))
+# tf.Tensor([[-0.20768732  0.13068241 -0.0526246 ]], shape=(1, 3), dtype=float32)
 
+print(dense_layer.weights)
+'''[<tf.Variable 'my_layer_2/Variable:0' shape=(5, 3) dtype=float32, numpy=
+array([[-0.00211058,  0.04550327, -0.05219404],
+       [ 0.01158094,  0.04858255,  0.06237484],
+       [-0.05346181,  0.06042183,  0.01163208],
+       [-0.05644415,  0.01921833, -0.07422426],
+       [-0.10725173, -0.04304357, -0.00021322]], dtype=float32)>, <tf.Variable 'my_layer_2/Variable:0' shape=(3,) dtype=float32, numpy=array([0., 0., 0.], dtype=float32)>]'''
 
-### We can even add more functionalities adding variables to customize variables.
-class MyLayerMean(Layer):
-    def __init__(self, units, input_dim):
-        super(MyLayerMean, self).__init__()
-        self.w = self.add_weight(shape=(input_dim, units), initializer='random_normal')
-        self.b = self.add_weight(shape=(units,), initializer='zeros')
-        self.sum_activation = tf.Variable(initial_value=tf.zeros((units,)), trainable=False)
-        self.number_call = tf.Variable(initial_value=0, trainable=False)
+dense_layer = MyLayer(3)
+x = tf.ones((1,1))
+print(dense_layer(x))
+# tf.Tensor([[ 0.03826848 -0.00380654 -0.04487282]], shape=(1, 3), dtype=float32)
 
-    def call(self, inputs):
-        activations = tf.matmul(inputs, self.w)+self.b
-        self.sum_activation.assign_add(tf.reduce_sum(activations, axis=0))
-        self.number_call.assign_add(inputs.shape[0])
-        return activations, self.sum_activation / tf.cast(self.number_call, tf.float32)
-
-dense_layer = MyLayerMean(3,5)
-
-# See how the values don't change (this is useful to follow the propagation of signals).
-y, activation_means = dense_layer(tf.ones((1,5)))
-print(activation_means.numpy())
-# [ 0.01249229  0.22859478 -0.03365123]
-
-print(activation_means.numpy())
-# [ 0.01249229  0.22859478 -0.03365123]
+print(dense_layer.weights)
+'''[<tf.Variable 'my_layer_8/Variable:0' shape=(1, 3) dtype=float32, numpy=
+array([[ 0.03826848, -0.00380654, -0.04487282]], dtype=float32)>, <tf.Variable 'my_layer_8/Variable:0' shape=(3,) dtype=float32, numpy=array([0., 0., 0.], dtype=float32)>]'''
 
 
-### Now we will create a Dropout layer and add it to a custom Model.
-class MyDropout(Layer):
-    def __init__(self, rate):
-        super(MyDropout, self).__init__()
-        self.rate = rate
-
-    def call(self, inputs):
-        return tf.nn.dropout(inputs, rate=self.rate) # here we are using the default Dropout layer defined in TF.
-
+# We can also benefit from flexible inputs when creating a model and passing outputs from layer to layer.
 class MyModel(Model):
-    def __init__(self, units_1, input_dim_1, units_2, units_3):
-        super(MyModel, self).__init__()
-        self.layer_1 = MyLayer(units_1, input_dim_1)
-        self.dropout_1 = MyDropout(0.5)
-        self.layer_2 = MyLayer(units_2, units_1)
-        self.dropout_2 = MyDropout(0.5)
-        self.layer_3 = MyLayer(units_3, units_2)
-        self.softmax = Softmax()
-
+    def __init__(self, units1, units2, **kwargs):
+        super(MyModel, self).__init__(**kwargs)
+        self.layer1 = MyLayer(units1)
+        self.layer2 = MyLayer(units2)
     def call(self, inputs):
-        x = self.layer_1(inputs)
+        x = self.layer1(inputs)
         x = tf.nn.relu(x)
-        x = self.dropout_1(x)
-        x = self.layer_2(x)
-        x = tf.nn.relu(x)
-        x = self.dropout_2(x)
-        x = self.layer_3(x)
+        x = self.layer2(x)
+        return Softmax()(x)
 
-        return self.softmax(x)
+model = MyModel(units1=32, units2=10)
+_ = model(tf.ones((1,100))) #to create and initialize all the weights
+model.summary()
 
-my_model = MyModel(64, 10000, 64, 46)
-print(my_model(tf.ones((1, 10000))))
-my_model.summary()
