b224162d6e1fe60f8593e852aa759026e8aea2d4
Ricardo Hernandez Ramon <richerram@gmail.com>
2021-01-02 22:48:39 -0600

    Custom Training Loops example

--- TF_pipeline.py
+++ TF_pipeline.py
@@ -1,71 +1,30 @@
-##### Automatic Differentiation #####
+##### Custom Training Loops example #####
 import tensorflow as tf
-from tensorflow.keras.layers import Layer
 from tensorflow.keras.models import Model
+from tensorflow.keras.losses import MeanSquaredError
+from tensorflow.keras.optimizers import SGD
 import numpy as np
-import matplotlib.pyplot as plt
 
-### We will apply custom differenciation to a simple Linear Regression problem.
-def MakeNoisyData(m, b, n=20):
-    x = tf.random.uniform(shape=(n,))
-    noise = tf.random.normal(shape=(len(x),), stddev=0.1)
-    y = m * x + b + noise
-    return x, y
-m = 1
-b = 2
-x_train, y_train = MakeNoisyData(m,b)
-plt.plot(x_train, y_train, 'b.')
+my_model = MyModel() #this is a model we have previously created using any known method.
 
-class LinearLayer(Layer):
-    def __init__(self):
-        super(LinearLayer, self).__init__()
-        self.m = self.add_weight(shape=(1,), initializer='random_normal')
-        self.b = self.add_weight(shape=(1, ), initializer='zeros')
-    def call(self, inputs):
-        return self.m * inputs + self.b
+def loss(y_hat, y):
+    return tf.reduce_mean(tf.square(y_hat - y))
+# the function above is the same as if we used the loss as part of TF library "loss = MeanSquaredError()".
 
-linear_regression = LinearLayer()
-print(linear_regression(x_train))
-# tf.Tensor(
-# [-1.8976429e-02 -1.7283266e-02 -7.7905636e-03 -2.2711808e-02
-#  -1.6863013e-02 -4.8839427e-03 -1.1285791e-02 -1.9753540e-02
-#  -8.3114393e-03 -2.7244675e-03 -2.6073331e-02 -1.0096144e-02
-#  -4.3933168e-03 -2.1872424e-02 -4.7077634e-03 -1.3091799e-02
-#  -2.6780302e-02 -2.2117294e-02 -2.8603491e-03 -5.3068448e-05], shape=(20,), dtype=float32)
-print(linear_regression.weights)
-# [<tf.Variable 'Variable:0' shape=(1,) dtype=float32, numpy=array([-0.03286118], dtype=float32)>,
-# <tf.Variable 'Variable:0' shape=(1,) dtype=float32, numpy=array([0.], dtype=float32)>]
+optimizer = SGD(learning_rate=0.05, momentum=0.9)
 
+epoch_losses = []
 
-### We have to manually define the "Loss Function"
-def SquaredError (y_pred, y_true):
-    return tf.reduce_mean(tf.square(y_pred-y_true))
-starting_loss = SquaredError(linear_regression(x_train), y_train)
-print('Starting Loss: {}'.format(starting_loss.numpy()))
-# Starting Loss: 5.90211820602417
+for epoch in range(num_epochs):
+    batch_losses = []
 
+    for inputs, outputs in training_dataset:
+        with tf.GradientTape() as tape:
+            current_loss = loss(my_model(inputs), outputs)
+            grads = tape.gradient(current_loss, my_model.trainable_variables)
 
-### Training and plotting the model
-learning_rate = 0.05
-steps = 25
+        batch_losses.append(current_loss)
+        optimizer.apply_gradients(zip(grads, my_model.trainable_variables))
 
-for i in range (steps):
-    with tf.GradientTape() as tape:
-        predictions = linear_regression(x_train)
-        loss = SquaredError(predictions, y_train)
-    gradients = tape.gradient(loss, linear_regression.trainable_variables)
-    linear_regression.m.assign_sub(learning_rate * gradients[0])
-    linear_regression.b.assign_sub(learning_rate * gradients[1])
+    epoch_losses.append(np.mean(batch_losses))
 
-    print('Step %d, Loss %f' % (i, loss.numpy()))
-'''Step 0, Loss 5.902118
-Step 1, Loss 4.605634
-Step 2, Loss 3.594527
-...
-Step 24, Loss 0.027175'''
-
-print('m:{}, trained m:{}'.format(m, linear_regression.m.numpy()))
-print('b:{}, trained b:{}'.format(b, linear_regression.b.numpy()))
-plt.plot(x_train, y_train, 'b.')
-x_linear_regression = np.linspace(min(x_train), max(x_train), 50)
-plt.plot(x_linear_regression, linear_regression.m * x_linear_regression + linear_regression.b, 'r.')
